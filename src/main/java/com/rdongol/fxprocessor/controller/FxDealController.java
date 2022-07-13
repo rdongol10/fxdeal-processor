48bd7983cf245ccf1db873657e503fab8312650b
Rabin Dongol <rawbean08@gmail.com>
2022-07-13 17:16:07 +0545

    added rest api for FXDeal

---
+++ src/main/java/com/rdongol/fxprocessor/controller/FxDealController.java
@@ -0,0 +1,25 @@
+package com.rdongol.fxprocessor.controller;
+
+import com.rdongol.fxprocessor.model.FxDeal;
+import com.rdongol.fxprocessor.service.FxDealProcessor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/fxDeal")
+public class FxDealController {
+
+    private FxDealProcessor fxDealProcessor;
+
+    public FxDealController(FxDealProcessor fxDealProcessor) {
+        this.fxDealProcessor = fxDealProcessor;
+    }
+
+    @PostMapping
+    public ResponseEntity<FxDeal> login(@RequestBody FxDeal fxDeal) {
+        return ResponseEntity.ok(fxDealProcessor.processFxDeal(fxDeal));
+    }
+}
---
+++ src/main/java/com/rdongol/fxprocessor/exceptionHandler/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.rdongol.fxprocessor.exceptionHandler;
+
+import com.rdongol.fxprocessor.exceptionHandler.exception.AppException;
+import com.rdongol.fxprocessor.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    protected static final Logger LOGGER = Logger.getLogger(GlobalExceptionHandler.class.getName());
+
+    @ExceptionHandler(AppException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<Object> handleAppException(AppException appException, WebRequest request) {
+        LOGGER.log(Level.WARNING, "App Exception", appException);
+        return buildErrorResponse(appException, HttpStatus.BAD_REQUEST);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception,
+                                                      HttpStatus httpStatus) {
+        return buildErrorResponse(exception, exception.getMessage(), httpStatus);
+    }
+
+    private ResponseEntity<Object> buildErrorResponse(Exception exception,
+                                                      String message,
+                                                      HttpStatus httpStatus
+    ) {
+        ErrorResponse errorResponse = new ErrorResponse(httpStatus.value(), message);
+
+        return ResponseEntity.status(httpStatus).body(errorResponse);
+    }
+
+
+}
