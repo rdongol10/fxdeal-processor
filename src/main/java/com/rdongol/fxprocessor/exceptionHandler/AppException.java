7d8c6eae6d9d847f71fddaf8ccd893156d502d3a
Rabin Dongol <rawbean08@gmail.com>
2022-07-13 16:35:03 +0545

    Added AppException

---
+++ src/main/java/com/rdongol/fxprocessor/exceptionHandler/AppException.java
@@ -0,0 +1,8 @@
+package com.rdongol.fxprocessor.exceptionHandler;
+
+public class AppException extends RuntimeException {
+    public AppException(String message) {
+        super(message);
+    }
+}
+
